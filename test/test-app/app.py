6984b7d14a75531654231cf9eb33bab2c7cb5a48
Jason Washburn <wburn@wburn.net>
2023-04-26 09:51:58 +0000

    adds duplicate header names test
    Signed-off-by: Jason Washburn <wburn@wburn.net>

--- test/test-app/app.py
+++ test/test-app/app.py
@@ -4,11 +4,44 @@ from spin_config import config_get
 from os import environ
 import toml
 
+
 def handle_request(request):
     if request.uri == "/foo":
         return Response(200,
                         {"content-type": "text/plain"},
                         bytes(f"foo indeed", "utf-8"))
+
+    if request.uri == "/duplicateheadertest":
+        test_headers = [("spin-header-test-key1", "value1"), ("spin-header-test-key2", "value2"),
+                        ("spin-header-test-key1", "value3"), ("spin-header-test-key2", "value4")]
+
+        key1_test_passes = request.headers.get(
+            "spin-header-test-key1") == "value1, value3"
+        key2_test_passes = request.headers.get(
+            "spin-header-test-key2") == "value2, value4"
+        if key1_test_passes and key2_test_passes:
+            response_content = "Duplicate Header Name Test: Pass"
+            response_code = 200
+
+        else:
+            example_curl_headers = '-H "spin-header-test-key1: value1" -H "spin-header-test-key2: value2" -H "spin-header-test-key1: value3" -H "spin-header-test-key2: value4"'
+            example_curl_request = f'curl {example_curl_headers} http://127.0.0.1:3000/duplicateheadertest'
+            required_headers = "\n".join(
+                [str(header) for header in test_headers])
+            response_content = f"""
+---------------------- Duplicate Header Name Test -------------------------------------
+To make this test pass, you must include the following headers in your request:
+{required_headers}
+
+Example Passing Curl Request: {example_curl_request}
+
+Actual Headers Received:
+{request.headers}
+---------------------------------------------------------------------------------------
+"""
+            response_code = 404
+
+        return Response(response_code, {"content-type": "text/plain"}, bytes(response_content, "utf-8"))
 
     print(f"Got request URI: {request.uri}")
 
