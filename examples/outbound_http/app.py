5e39adf59a3104cf55abd333369942685479d5c0
Sohan <1119120+sohanmaheshwar@users.noreply.github.com>
2023-06-15 13:07:35 +0200

    Update app.py
    The previous dog-facts API was not working.
    
    Updated code to include working API. This API returns animal facts and not only dog facts. 
    
    See GH issue here: https://github.com/fermyon/developer/issues/692
    
    Signed-off-by: Sohan <1119120+sohanmaheshwar@users.noreply.github.com>

--- examples/outbound_http/app.py
+++ examples/outbound_http/app.py
@@ -4,8 +4,8 @@ from spin_http import Request, Response, http_send
 def handle_request(request):
 
     response = http_send(
-        Request("GET", "https://some-random-api.ml/facts/dog", {}, None))
+        Request("GET", "https://random-data-api.fermyon.app/animals/json", {}, None))
 
     return Response(200,
                     {"content-type": "text/plain"},
-                    bytes(f"Here is a dog fact: {str(response.body, 'utf-8')}", "utf-8"))
+                    bytes(f"Here is an animal fact: {str(response.body, 'utf-8')}", "utf-8"))
