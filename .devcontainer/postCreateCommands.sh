22ec95900ee0e34a1301ba75d1d4add523f1a4f5
Jason Washburn <jason.washburn@gmail.com>
2023-07-08 12:47:48 +0000

    build spin from source in devcontainer
    Signed-off-by: Jason Washburn <jason.washburn@gmail.com>

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Build cPython
+# Build cPython for wasm32
 git submodule update --init --recursive
 mkdir -p cpython/builddir/wasi
 mkdir -p cpython/builddir/build
 cd cpython/builddir/build
 ../../configure --prefix=$(pwd)/install --enable-optimizations
-make
+make -j$(nproc) -s
 cd ../wasi
 CONFIG_SITE=../../Tools/wasm/config.site-wasm32-wasi ../../Tools/wasm/wasi-env \
     ../../configure -C --host=wasm32-unknown-wasi --build=$(../../config.guess) \
         --with-build-python=$(pwd)/../build/python --prefix=$(pwd)/install --disable-test-modules
-make
+make -j$(nproc) -s
 make install
 cd ../../..
-
-# Build spin-python-cli
-make
