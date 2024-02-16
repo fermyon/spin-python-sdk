e5f2abb4c8e2fefda1f291e6c47ad5b70f3f00f7
karthik2804 <karthik.ganeshram@fermyon.com>
2024-02-16 13:34:11 -0800

    rename parameter to match WIT
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

--- examples/spin-llm/app.py
+++ examples/spin-llm/app.py
@@ -6,7 +6,7 @@ class IncomingHandler(IncomingHandler):
         res = llm.infer("llama2-chat", "tell me a joke")
         print(res.text)
         print(res.usage)
-        res = llm.infer_with_options("llama2-chat", "what is the theory of relativity", llm.LLMInferencingParams(temperature=0.5))
+        res = llm.infer_with_options("llama2-chat", "what is the theory of relativity", llm.InferencingParams(temperature=0.5))
         print(res.text)
         print(res.usage)
         return Response(
