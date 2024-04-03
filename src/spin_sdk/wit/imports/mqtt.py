057859e2e84f59c745a3ff6010434f7433d73e93
karthik2804 <karthik.ganeshram@fermyon.com>
2024-04-03 12:52:17 -0700

    add outbound mqtt bindings
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

---
+++ src/spin_sdk/wit/imports/mqtt.py
@@ -0,0 +1,75 @@
+from typing import TypeVar, Generic, Union, Optional, Protocol, Tuple, List, Any, Self
+from enum import Flag, Enum, auto
+from dataclasses import dataclass
+from abc import abstractmethod
+import weakref
+
+from ..types import Result, Ok, Err, Some
+
+
+
+@dataclass
+class Error_InvalidAddress:
+    pass
+
+
+@dataclass
+class Error_TooManyConnections:
+    pass
+
+
+@dataclass
+class Error_ConnectionFailed:
+    value: str
+
+
+@dataclass
+class Error_Other:
+    value: str
+
+
+Error = Union[Error_InvalidAddress, Error_TooManyConnections, Error_ConnectionFailed, Error_Other]
+"""
+Errors related to interacting with Mqtt
+"""
+
+
+class Qos(Enum):
+    """
+    QoS for publishing Mqtt messages
+    """
+    AT_MOST_ONCE = 0
+    AT_LEAST_ONCE = 1
+    EXACTLY_ONCE = 2
+
+class Connection:
+    
+    @classmethod
+    def open(cls, address: str, username: str, password: str, keep_alive_interval_in_secs: int) -> Self:
+        """
+        Open a connection to the Mqtt instance at `address`.
+        
+        Raises: `spin_sdk.wit.types.Err(spin_sdk.wit.imports.mqtt.Error)`
+        """
+        raise NotImplementedError
+
+    def publish(self, topic: str, payload: bytes, qos: Qos) -> None:
+        """
+        Publish an Mqtt message to the specified `topic`.
+        
+        Raises: `spin_sdk.wit.types.Err(spin_sdk.wit.imports.mqtt.Error)`
+        """
+        raise NotImplementedError
+
+    def __enter__(self):
+        """Returns self"""
+        return self
+                                                                    
+    def __exit__(self, *args):
+        """
+        Release this resource.
+        """
+        raise NotImplementedError
+
+
+
